382ed100faab177ba4b3cbb6d9e5bdac3383aecc
timiofearth <55197962+Timi-of-Earth@users.noreply.github.com>
2022-07-04 20:06:31 +0000

    I have solved all three challenges. The solutions passed the tests.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  arrayOfNumbers.forEach((num) => {
+    sum += num;
+  })
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  arrayOfNumbers.forEach((num) => {
+    if (num %2 === 0) {
+      count += 1;
+    }
+  })
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +57,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let farenheitArray = [];
+  arrayOfNumbers.forEach((num) => {
+    let converted = Math.floor((num * 1.8) + 32);
+    farenheitArray.push(converted);
+  })
+    return farenheitArray;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
